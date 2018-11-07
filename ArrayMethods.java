adb487f42ad931ff675a136339db1783398d2055
sxiao1 <sxiao1@stuy.edu>
2018-11-06 22:55:19 -0500

    final

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,8 +1,11 @@
 public class ArrayMethods{
   public static int rowSum(int[][] ary, int x){
     int sum = 0;
+    if(x >= ary.length){
+      return sum;
+    }
     for(int i = 0; i < ary[x].length; i ++){
-      sum += ((ary[x])[i]);
+      sum += ary[x][i];
     }
     return sum;
   }
@@ -10,13 +13,28 @@ public class ArrayMethods{
     int sum = 0;
     for(int i = 0; i < ary.length; i++){
       if(ary[i].length > x){
-        sum += (ary[i])[x];
+        sum += ary[i][x];
       }
       else{
         sum += 0;
       }
     }
   return sum;
+}
+  public static int[] allRowSums(int[][] ary){
+    int[] ary2 = new int [ary.length];
+    for(int i = 0; i < ary2.length; i++){
+      ary2[i] = rowSum(ary, i);
+    }
+    return ary2;
   }
+  public static int[] allColSums(int[][] ary){
+    int [] ary2 = new int [ary.length];
+    for(int i = 0; i < ary.length; i++){
+      ary2[i] = columnSum(ary,i);
+    }
+    return ary2;
+  }
+
 
 }
